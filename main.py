d39ab4c03d90da93c5d54815fedfb6774d39dd8e
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-08-18 20:05:12 +0530

    add count down timer and alarm timer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import math
 from tkinter import *
 from tkinter import ttk
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,8 +15,29 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def timer_count_down():
+    count_down(5, lambda : alarm_count_down(5 * 60))
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count, on_finish=None):
+    canvas.itemconfig(count_down_timer, text=count)
+    if count != 0:
+        windows.after(1000, count_down, count-1, on_finish)
+    else:
+        canvas.itemconfig(count_down_timer, text="")
+        if on_finish:
+            on_finish()
+
+def alarm_count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(alarm_timer, text=f"0{count_min}:{count_sec}")
+    if count > 0:
+        windows.after(1000, alarm_count_down, count-1)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 windows = Tk()
@@ -29,6 +52,7 @@ moon_img = PhotoImage(file="images/clock-green.png")
 oak_img = PhotoImage(file="images/growing-seed.png")
 play_img = PhotoImage(file="images/play.png")
 pause_img = PhotoImage(file="images/pause.png")
+checkbox = PhotoImage(file="images/checkbox.png")
 
 
 canvas = Canvas(windows, width=200, height=220, bg="white", highlightthickness=0)
@@ -37,11 +61,12 @@ canvas.place(relx=0.5, y=15, anchor="n")
 canvas.create_image(100, 100, image=moon_img)
 canvas.create_image(110, 100, image=oak_img)
 
-canvas.create_text(97, 205, text="00:00", fill="black", font=(FONT_NAME, 25, "bold"))
+count_down_timer = canvas.create_text(97, 100, text="", fill="white", font=(FONT_NAME, 40, "bold"))
+alarm_timer = canvas.create_text(97, 200, text="", fill="black", font=(FONT_NAME, 25, "bold"))
 
 
 button_frame = Frame(windows, bg="white")
-button_frame.place(relx=0.46, y=270, anchor="center")
+button_frame.place(relx=0.46, y=300, anchor="center")
 
 
 pause_button = Button(button_frame, image=pause_img, bg="white",
@@ -52,9 +77,18 @@ pause_button.pack(side="left", padx=10)
 
 play_button = Button(button_frame, image=play_img, bg="white",
                      activebackground="white", borderwidth=0,
-                     command=lambda: print("Play"), highlightthickness=0)
+                     command=timer_count_down, highlightthickness=0)
 play_button.image = play_img
 play_button.pack(side="left")
+
+
+label_frame = Frame(windows, bg="white")
+label_frame.place(relx=0.49, y =250, anchor="center")
+
+image_label = Label(label_frame, image=checkbox,bg="white", highlightthickness=0)
+image_label.pack(side="left")
+
+
 
 
 
