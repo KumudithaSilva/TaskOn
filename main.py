426cfa8707c3f809474404e3c3f863be11281948
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-08-26 20:27:53 +0530

    CI main change

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ def main():
     root.geometry("250x350")
     root.config(padx=5, pady=5, bg="#fdfdfd")
     root.resizable(False, False)
-    root.iconbitmap("images/growing-seed.ico")
+    # root.iconbitmap("images/growing-seed.ico")
 
-    loader = ResourceLoader()
-    images = {
-        "moon": loader.load_image("clock-green.png"),
-        "oak": loader.load_image("growing-seed.png"),
-        "play": loader.load_image("play.png"),
-        "pause": loader.load_image("pause.png"),
-        "checkbox": loader.load_image("checkbox.png"),
-        "work": loader.load_image("studying.png"),
-        "short_break": loader.load_image("short_break.png"),
-        "long_break": loader.load_image("long_break.png"),
-    }
-
-    app = TaskOnUI(root, images)
+    # loader = ResourceLoader()
+    # images = {
+    #     "moon": loader.load_image("clock-green.png"),
+    #     "oak": loader.load_image("growing-seed.png"),
+    #     "play": loader.load_image("play.png"),
+    #     "pause": loader.load_image("pause.png"),
+    #     "checkbox": loader.load_image("checkbox.png"),
+    #     "work": loader.load_image("studying.png"),
+    #     "short_break": loader.load_image("short_break.png"),
+    #     "long_break": loader.load_image("long_break.png"),
+    # }
+    #
+    # app = TaskOnUI(root, images)
     root.mainloop()
 
 
--- resources.py
+++ resources.py
@@ -2,8 +2,8 @@ import os
 from tkinter import PhotoImage
 
 class ResourceLoader:
-    def __init__(self, base_path=None):
-        self.base_path = base_path or os.path.join(os.path.dirname(__file__), "images")
+    def __init__(self, base_path="images/"):
+        self.base_path = base_path
 
     def load_image(self, filename: str):
         path = os.path.join(self.base_path, filename)
