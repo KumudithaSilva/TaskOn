73b3793427679f5fb2e1dbe8c3614e2b02461607
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-08-30 03:02:06 +0530

    add taskonapp

---
+++ app.py
@@ -0,0 +1,10 @@
+from timer import Timer
+from audio_service import  PygameAudionService
+from ui import TaskOnUI
+
+class TaskOnApp:
+    WORK_MIN = 1
+    SHORT_BREAK_MIN = 1
+    LONG_BREAK_MIN = 1
+    START_DELAY = 5
+
